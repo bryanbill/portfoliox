ce469b0aa354a36e26c32acdbb773b72289519d8
bryanbill <emobrya@gmail.com>
2022-03-04 02:20:38 +0300

    chore:cleaned up the code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 const openLink = (link) => {
     window.open("https://github.com/bryanbill/" + link, '_blank');
 }
+const isDark = false;
+const changeTheme = () => {
+    if (isDark) {
+        document.documentElement.classList.remove("bg-dark");
+        document.documentElement.classList.add("bg-light");
+        isDark = false;
+    }
+    else {
+        document.documentElement.classList.remove("bg-light");
+        document.documentElement.classList.add("bg-dark");
+        isDark = true;
+    }
+    console.log(isDark);
+}
